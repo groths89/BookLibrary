0a27c8cf0d4c52e927c2e1a152bb91f7a1c43bdd
Gregory Rothstein <gregoryrothstein@Gregorys-AiCure-MacBook-Pro-6.local>
2019-11-11 14:01:50 -0500

    updated image paths

--- src/app/sample-books.ts
+++ src/app/sample-books.ts
@@ -5,30 +5,30 @@ export const BOOKS: Book[] = [
         id: 1,
         bookTitle: 'Treasure Island',
         bookAuthor: 'Robert Louis Stevenson',
-        bookImage: '../assets/BookImages/treasure-island.jpg'
+        bookImage: './assets/BookImages/treasure-island.jpg'
     },
     {
         id: 2,
         bookTitle: 'The Great Gatsby',
         bookAuthor: 'F. Scott Fitzgerald',
-        bookImage: '../assets/BookImages/great-gatsby.jpg'
+        bookImage: './assets/BookImages/great-gatsby.jpg'
     },
     {
         id: 3,
         bookTitle: 'The Tale of Two Cities',
         bookAuthor: 'Charles Dickens',
-        bookImage: '../assets/BookImages/tale-of-two-cities.jpeg'
+        bookImage: './assets/BookImages/tale-of-two-cities.jpeg'
     },
     {
         id: 4,
         bookTitle: 'Charlotte\'s Web',
         bookAuthor: 'E. B. White',
-        bookImage: '../assets/BookImages/charlotte-web.jpg'
+        bookImage: './assets/BookImages/charlotte-web.jpg'
     },
     {
         id: 5,
         bookTitle: 'Kidnapped',
         bookAuthor: 'Robert Louis Stevenson',
-        bookImage: '../assets/BookImages/kidnapped.jpg'
+        bookImage: './assets/BookImages/kidnapped.jpg'
     }
 ];
